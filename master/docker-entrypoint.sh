b4c2f06b6f1c30e0c9cdc485fcb2703156c5c064
tsai <jackalcooper@gmail.com>
2023-05-11 10:29:07 +0800

    fix

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
-sudo sed -i "s/slurmmaster/#{HEAD_HOST}/g" /etc/slurm-llnl/slurm.conf
+sudo sed -i "s/slurmmaster/${HEAD_HOST}/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
