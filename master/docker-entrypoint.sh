605b3f6f21a94aae389de0c79301aebc1cceb022
tsai <jackalcooper@gmail.com>
2023-05-11 12:36:32 +0800

    refine

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -2,13 +2,6 @@
 set -e
 
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
-sudo echo "${HEAD_HOST} slurmmaster" >> /etc/hosts
-
-IFS="," read -ra arr <<< "$WORKER_HOSTS"
-for i in "${!arr[@]}"
-do
-    sudo echo "${arr[i]} slurmnode$((i+1))" >> /etc/hosts
-done
 
 sudo service munge start
 sudo service slurmctld start
--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -2,9 +2,8 @@
 set -e
 
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
-sudo echo "${HEAD_HOST} slurmmaster" >> /etc/hosts
 
 sudo service munge start
 sudo slurmd -N $(hostname)
 
-tail -f /dev/null
+tail -f /var/log/slurm-llnl/slurmd.log
