7d63a5ef0b0f2b468ad03e4c767def1ad1c815a2
tsai <jackalcooper@gmail.com>
2023-05-11 13:54:35 +0800

    refine

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
-sudo slurmd -N $(hostname)
+sudo slurmd -N ${SLURM_NODENAME}
 
 tail -f /var/log/slurm-llnl/slurmd.log
