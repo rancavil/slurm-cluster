d9a32531dffe7a7bda25075e883a09e1d1d761d1
Jiarui Fang <fangjiarui123@gmail.com>
2023-05-16 11:43:39 +0800

    update ip list on Tencent Clould (#4)

--- gn/launch_master.sh
+++ gn/launch_master.sh
@@ -10,8 +10,8 @@ docker run -d --rm --name slurm-head-$USER --network=host \
 --add-host=slurmmaster:10.1.0.6 \
 --add-host=slurmnode1:10.0.0.9 \
 --add-host=slurmnode2:10.0.0.10 \
---add-host=slurmnode3:10.0.0.11 \
---add-host=slurmnode4:10.0.0.13 \
+--add-host=slurmnode3:10.0.0.13 \
+--add-host=slurmnode4:10.0.0.17 \
 -v /mnt:/mnt \
 -v /data_32T:/data_32T \
 -v /data_160T:/data_160T \
