5c9c7451caa1e4ebe8360e49ec0ad68f880200a8
Richard Vencu <vencu.richard@gmail.com>
2023-10-27 12:32:43 +0300

    customization

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
+sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /opt/slurm/etc/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
